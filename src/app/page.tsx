4f493ab27bcf573dd9b7f3396ee2179b2d236038
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2024-02-18 15:52:24 -0800

    work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,227 @@
-import Image from 'next/image'
+import { YouTubeEmbed } from "@next/third-parties/google";
+import { GitHub, Twitter } from "./icons";
+
+type CareerItem = {
+  company: React.ReactNode;
+  title: React.ReactNode;
+  product: React.ReactNode;
+  date: React.ReactNode;
+  description: React.ReactNode;
+};
+const carerItems: CareerItem[] = [
+  {
+    company: "Microsoft",
+    date: "August 2023 - Present",
+    product: "Teams",
+    description: "",
+    title: "Software Engineer",
+  },
+  {
+    company: "Microsoft",
+    date: "May 2022 - August 2022",
+    product: "Teams",
+    description: "",
+    title: "Software Engineer Intern",
+  },
+  {
+    company: "Raven Software",
+    date: "May 2021 - August 2021",
+    product: "Warzone",
+    description: "",
+    title: "Gameplay Programmer Intern",
+  },
+  {
+    company: "Epic Games",
+    date: "May 2020 - August 2020",
+    product: "Fortnite",
+    description: "",
+    title: "Gameplay Programmer Optimization Intern",
+  },
+];
+
+type ProjectItem = {
+  name: React.ReactNode;
+  description: React.ReactNode;
+  date: React.ReactNode;
+  url: string;
+};
+
+const projectItems: ProjectItem[] = [
+  {
+    name: "Answer Overflow",
+    description:
+      "Open source webpages for Discord forum channels. Bootstrapped to 350k MAU, built with Next.js, TypeScript, and Turborepo, tRPC, and Drizzle.",
+    url: "https://www.answeroverflow.com",
+    date: "January 2022 - Present",
+  },
+  {
+    name: "Typelytics",
+    description:
+      "Typescript library for analytics querying & rendering of PostHog data",
+    url: "https://typelytics.rhyssul.com",
+    date: "January 2024 - Present",
+  },
+];
+
+type Misc = {
+  name: string;
+  description: string;
+  date: string;
+  teamSize: string;
+  duration: string;
+  videoid: string;
+};
+
+const misc: Misc[] = [
+  {
+    name: "Shelf Life",
+    description:
+      " iOS / Android app built with React Native to digitize a pantry. Used Python, Flask, and SQL for the backend. App tracks nutritional information, expiration dates, budget, and suggests recipes",
+    teamSize: "4",
+    duration: "2 months",
+    videoid: "kMtXJYT95oc",
+    date: "Feburary - April 2021",
+  },
+  {
+    name: "Into the Depths",
+    description:
+      "Singleplayer VR game where you pilot a submarine and fight a flying water dragon. Submarine had missiles and sonar for detecting floating mines.",
+    date: "December 2020",
+    duration: "7 days",
+    teamSize: "5",
+    videoid: "UQY_y9FIhzM",
+  },
+  {
+    name: "Ground Control",
+    date: "December 2019",
+    description: `2 player game made for the theme "Down To Earth". One player is in a Spaceship trying to return to earth and survive reentry as their ship falls apart. The other player is in ground control telling the player in the Spaceship where their issues are and how to fix them.`,
+    duration: "7 days",
+    teamSize: "5",
+    videoid: "lJHS-Mf3bMo",
+  },
+  {
+    name: "Busy Bee Movers",
+    date: "ArtCenter GameJam 2021",
+    description:
+      "2 Player LAN / Online Co-Op game. Both players control one character, if they move together they go full speed, if one person is only moving they go half speed, and if they do opposite inputs they don't move at all.",
+    duration: "48 hours",
+    teamSize: "4",
+    videoid: "du1VECmZ8L8",
+  },
+  {
+    date: "January - June 2019",
+    description:
+      "Virtual Reality Kart Racer that I made to learn VR, C++, and Networking",
+    duration: "6 months",
+    name: "VR Kart Racer",
+    teamSize: "1",
+    videoid: "LZSft4THWVQ",
+  },
+  {
+    name: "VR Spin Bike Project",
+    date: "April 2020",
+    description:
+      "VR project running on the Oculus Quest that converts spin bike pedaling into in-game input. You race 60 other bikes in a lap around an island.",
+    duration: "2 weeks",
+    teamSize: "1",
+    videoid: "E6VWXjUquZo",
+  },
+];
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+    <main className="flex text-white bg-zinc-950 min-h-screen flex-col items-center min-w-full">
+      <div className="max-w-3xl w-full ">
+        <section className="py-10 flex flex-row justify-between">
+          <div>
+            <h1 className="font-bold text-xl p-0 m-0 ">Rhys Sullivan</h1>
+            <span className="text-neutral-200 p-0 m-0">Product Engineer</span>
+          </div>
+          <div className="flex gap-4">
+            <a
+              href="https://github.com/rhyssullivan"
+              target="_blank"
+              className="fill-current hover:fill-neutral-300"
+              aria-label="Twitter"
+            >
+              <GitHub />
+            </a>
+            <a
+              href="https://twitter.com/rhyssullivan"
+              target="_blank"
+              className="fill-current hover:fill-neutral-300"
+              aria-label="Twitter"
+            >
+              <Twitter />
+            </a>
+          </div>
+        </section>
+        <section className="text-left w-full flex gap-4 flex-col">
+          <h2 className="text-xl font-bold">Career</h2>
+          {carerItems.map((item, index) => (
+            <div key={index}>
+              <div className="min-w-full flex flex-row justify-between">
+                <div className="flex flew-row">
+                  <h3 className="font-bold">{item.company}</h3>
+                  <span className="mx-2 font-bold">-</span>
+                  <span className="font-bold">{item.product}</span>
+                  <i className="mx-2">{item.title}</i>
+                </div>
+                <div>
+                  <span>{item.date}</span>
+                </div>
+              </div>
+              <span>{item.description}</span>
+            </div>
+          ))}
+        </section>
+        <section className="text-left w-full flex gap-4 flex-col mt-4 py-4 border-t-2 border-zinc-800 rounded-e-md">
+          <h2 className="text-xl font-bold">Active Projects</h2>
+          {projectItems.map((item, index) => (
+            <div key={index} className="py-4">
+              <div className="min-w-full flex flex-row justify-between">
+                <div className="flex flew-row">
+                  <a href={item.url} target="_blank">
+                    <h3
+                      className="font-bold
+                    hover:text-neutral-300
+                    hover:underline
+                    "
+                    >
+                      {item.name}
+                    </h3>
+                  </a>
+                </div>
+                <div>
+                  <span>{item.date}</span>
+                </div>
+              </div>
+              <span>{item.description}</span>
+            </div>
+          ))}
+        </section>
+        <section className="text-left w-full flex gap-4 flex-col mt-4 py-4 border-t-2 border-zinc-800 rounded-e-md">
+          <h2 className="text-xl font-bold">Past Projects</h2>
+          {misc.map((item, index) => (
+            <div key={index} className="pb-8">
+              <div className="min-w-full flex flex-row justify-between">
+                <div className="flex flew-row">
+                  <h3 className="font-bold">{item.name}</h3>
+                </div>
+                <div>
+                  <span>{item.date}</span>
+                </div>
+              </div>
+              <div className="flex flex-col gap-4">
+                <span>{item.description}</span>
+                <span>Team Size: {item.teamSize}</span>
+                <span>Duration: {item.duration}</span>
+              </div>
+              <YouTubeEmbed videoid={item.videoid} />
+            </div>
+          ))}
+        </section>
       </div>
     </main>
-  )
+  );
 }
